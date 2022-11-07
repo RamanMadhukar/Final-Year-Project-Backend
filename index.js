adcc017cf1373ee3c1e9b8f58c2d3ed33da03e0a
Raman Madhukar <ramanmadhukar6@gmail.com>
2022-11-07 22:57:19 +0530

    updating cors

--- index.js
+++ index.js
@@ -12,23 +12,29 @@ app.use(express.json());
 
 app.use(
     cors({
-      origin: ["https://zingy-heliotrope-c26a4b.netlify.app"],
-      methods: ["GET", "POST", "DELETE"],
-      credentials: true,
-      origin: true,
+        origin: ["https://zingy-heliotrope-c26a4b.netlify.app"],
+        methods: ["GET", "POST", "DELETE"],
+        credentials: true,
+        origin: true,
     })
-  );
+);
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(routes);
 
-mongoose.connect(process.env.CONNECTION_URI,{
+mongoose.connect(process.env.CONNECTION_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-        console.log("Database Connected");
-    }).catch((e) => {
-        console.log("database Not Connected",e);
-    });
+    console.log("Database Connected");
+}).catch((e) => {
+    console.log("database Not Connected", e);
+});
 
 
 
