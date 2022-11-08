27afc12caf0dd982c40b2b33a866aa2d8c8544c9
Raman Madhukar <ramanmadhukar6@gmail.com>
2022-11-08 10:24:22 +0530

    cehcking routes 3

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,31 +1,37 @@
 const querySchema = require('../Database/patient')
 
 exports.postPatient = (req, res) => {
-    // const { name, age, disease } = req.body.patient;
-    // const { lat, lng } = req.body.position;
-    // const query = new querySchema({ name, age, disease, lat, lng });
+    const { name, age, disease } = req.body.patient;
+    const { lat, lng } = req.body.position;
+    const query = new querySchema({ name, age, disease, lat, lng });
 
-    // query.save((err, data) => {
-    //     if (err) {
-    //         console.log('error', err);
-    //         return res.status(400).json({
-    //             error: 'Error saving employee in database. Try again'
-    //         });
-    //     }
-    //     return res.json({
-    //         message: 'data saved'
-    //     });
-    // });
+    query.save((err, data) => {
+        if (err) {
+            console.log('error', err);
+            return res.status(400).json({
+                error: 'Error saving employee in database. Try again'
+            });
+        }
+        return res.json({
+            message: 'data saved'
+        });
+    });
     return res.send("post triggred");
 }
 
 exports.getPatient = (req, res) => {
 
-    // querySchema.find(function (err, found) {
-    //     return res.json(found);
-    // });
+    querySchema.find(function (err, found) {
+        if(found){
+            return res.json(found);
 
-        return res.send("found");
+        }
+        else{
+            return res.send(err);
+        }
+    });
+
+        // return res.send("found");
 
 
 }
--- Database/patient.js
+++ Database/patient.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const querySchema = mongoose.Schema(
+const querySchema = new mongoose.Schema(
     {
         name:{type:String},
         age:{type:Number},
